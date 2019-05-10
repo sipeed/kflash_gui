8d58434be6230908e28ced49e15fbe77eab38ccf
Neucrack <CZD666666@gmail.com>
2019-05-10 22:06:05 +0800

    add pack

---
+++ pack.py
@@ -0,0 +1,9 @@
+import os, sys, shutil
+
+shutil.rmtree("kflash_py/__pycache__")
+if sys.platform == "win32":
+    cmd = 'pyinstaller --add-data="kflash_gui_data;kflash_gui_data" --add-binary="kflash_py;kflash_py" -i="kflash_gui_data/assets/logo.ico" -w kflash_gui.py'
+else:
+    cmd = 'pyinstaller --add-data="kflash_gui_data:kflash_gui_data" --add-binary="kflash_py:kflash_py" -i="kflash_gui_data/assets/logo.ico" -w kflash_gui.py'
+os.system(cmd)
+
