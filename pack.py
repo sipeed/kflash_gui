17253ecd1b27fc99d4e7cf5416adac4f797fa473
Neucrack <CZD666666@gmail.com>
2019-05-11 01:06:39 +0800

    update ico

--- pack.py
+++ pack.py
@@ -1,6 +1,7 @@
 import os, sys, shutil
 
-shutil.rmtree("kflash_py/__pycache__")
+if os.path.exists("kflash_py/__pycache__"):
+    shutil.rmtree("kflash_py/__pycache__")
 if sys.platform == "win32":
     cmd = 'pyinstaller --add-data="kflash_gui_data;kflash_gui_data" --add-binary="kflash_py;kflash_py" -i="kflash_gui_data/assets/logo.ico" -w kflash_gui.py'
 else:
