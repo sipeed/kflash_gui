aa9241dbad6c26bdff9f72c6b017cbf98c60ce1d
Orgmar <orgmars@gmail.com>
2019-06-17 22:59:34 +0800

    Add macOS support
    Add macOS icon file

--- pack.py
+++ pack.py
@@ -4,7 +4,9 @@ if os.path.exists("kflash_py/__pycache__"):
     shutil.rmtree("kflash_py/__pycache__")
 if sys.platform == "win32":
     cmd = 'pyinstaller --add-data="kflash_gui_data;kflash_gui_data" --add-binary="kflash_py;kflash_py" -i="kflash_gui_data/assets/logo.ico" -w kflash_gui.py'
+elif sys.platform == "darwin":
+    cmd = 'pyinstaller --add-data="kflash_gui_data:kflash_gui_data" --add-data="kflash_py:kflash_py" -i="kflash_gui_data/assets/logo.icns" -w kflash_gui.py'
 else:
-    cmd = 'pyinstaller --add-data="kflash_gui_data:kflash_gui_data" --add-binary="kflash_py:kflash_py" -i="kflash_gui_data/assets/logo.ico" -w kflash_gui.py'
+    cmd = 'pyinstaller --add-data="kflash_gui_data:kflash_gui_data" --add-data="kflash_py:kflash_py" -i="kflash_gui_data/assets/logo.png" -w kflash_gui.py'
 os.system(cmd)
 
