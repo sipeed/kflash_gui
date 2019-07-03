1311d7df46296cd4f9136e40cc37a70e16e76b6f
Neucrack <czd666666@gmail.com>
2019-07-03 12:03:23 +0800

    optimize help info

--- helpAbout.py
+++ helpAbout.py
@@ -25,7 +25,7 @@ def strAbout():
     py_version = sys.version_info
     return '''\
 Python ''' +str(py_version[0]+py_version[1]/10)+ ''' + PyQt5<br><br>
-<div><div>'''+parameters.appName+tr("is a Open source project created by")+''' </div><a style="vertical-align: middle;" href="http://www.sipeed.com"><img src="'''+strPath+"/"+parameters.appLogo2+'''" width=109 height=32></img></a><br></div>
+<div><div>'''+parameters.appName+" "+tr("is a Open source project created by")+''' </div><a style="vertical-align: middle;" href="http://www.sipeed.com"><img src="'''+strPath+"/"+parameters.appLogo2+'''" width=109 height=32></img></a><br></div>
 ''' +tr("Author")+":"+parameters.author+'''<br><br>
 
 '''+tr("See more on")+''' <b><a href="https://github.com/Sipeed/kflash_gui.git">Github</a></b>,  '''+tr("Licensed with")+''' <a href="https://github.com/sipeed/kflash_gui/blob/master/LICENSE">LGPL3.0</a><br><br>
