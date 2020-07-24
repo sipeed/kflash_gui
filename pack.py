727aec6984f5691960ce368551ddbb2b5d15eb24
Huang Rui <vowstar@gmail.com>
2020-07-24 17:01:17 +0800

    pack.py: Add windows pack script
    Both bash.exe and 7z.exe are needed
    
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- pack.py
+++ pack.py
@@ -25,7 +25,14 @@ if result != 0:
 
 # create packages
 if sys.platform.startswith("win32"):
-    pass
+    if os.path.exists("./dist/kflash_gui.7z"):
+        os.remove("./dist/kflash_gui.7z")
+    cmd = """bash.exe -c \
+        " \
+        cd ./dist || exit -1 ; \
+        7z a "dist/kflash_gui.7z" "dist/kflash_gui" -bd -mx9 || exit -1 ; \
+        " \
+    """
 elif sys.platform.startswith("darwin"):
     if os.path.exists("./dist/kflash_gui.dmg"):
         os.remove("./dist/kflash_gui.dmg")
