73d96ff689cb341bd74f6ebe5e1dda7512dccf15
Huang Rui <vowstar@gmail.com>
2022-05-27 15:57:48 +0800

    helpAbout.py: bump version to 1.7.4
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- helpAbout.py
+++ helpAbout.py
@@ -6,9 +6,9 @@ import time
 
 versionMajor = 1
 versionMinor = 7
-versionDev   = 3
+versionDev   = 4
 
-date = "2022.05.25"
+date = "2022.05.27"
 
 def strAbout():
     pathDirList = sys.argv[0].replace("\\", "/").split("/")
